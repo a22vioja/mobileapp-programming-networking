7eb6f97662355b1b3031cb9398f98c8e403525a7
a22vioja <a22vioja@student.his.se>
2023-04-25 16:43:26 +0200

    created java class for representing the item

--- app/Mountain.java
+++ app/Mountain.java
@@ -1,2 +1,15 @@
 public class Mountain {
+    @SuppressWarnings("WeakerAccess")
+    public class RecyclerViewItem {
+
+        private String title;
+
+        public RecyclerViewItem(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
 }
